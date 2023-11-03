d7cf25a51f6280665bef9341709db6ca744cbed9
MRDK369 <contactme2486@gmail.com>
2023-11-03 13:22:58 +0530

    t and ml

--- ai.py
+++ ai.py
@@ -6,3 +6,7 @@
 #limited memory ai is works giving input past work in present eg:tesla car 
 #theory of mind ai is works observe emotion give the result eg:dolls and toy  robot
 #self aware ai is works protect myself eg: ("transformers")movie  
+
+#traditionl ai and ml (machine learning)
+#T ai works give  a words input and ai will process 
+#ml works give a eg to give a input to machine . machine automatically works.    
