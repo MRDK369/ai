2cacbd0efee7c373cec291969a496d8f276672bc
MRDK369 <contactme2486@gmail.com>
2023-11-03 13:13:38 +0530

    of ai

--- ai.py
+++ ai.py
@@ -1,7 +1,8 @@
 #in 1951 christopher strachey invent ai
 
 #Type of ai 
-#reactive ai 
-#limited memory ai
-#theory of mind ai
-#self aware ai
+#reactive ai is  pre work system eg:chess 
+
+#limited memory ai is works giving input past work in present eg:tesla car 
+#theory of mind ai is works observe emotion give the result eg:dolls and toy  robot
+#self aware ai is works protect myself eg: ("transformers")movie  
